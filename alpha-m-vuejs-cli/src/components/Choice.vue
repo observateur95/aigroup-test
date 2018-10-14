07158305d61614ff2e21f74e0920a5f4e70515e1
DanielPopOut <stephane.tchangang@gmail.com>
2018-10-14 13:31:11 +0100

    DT changed datas

---
+++ alpha-m-vuejs-cli/src/components/Choice.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="choice-div" v-on:click="$emit('choice-click',index)">{{ txtMessage }}</div>
+</template>
+
+<script>
+    export default {
+        name: 'Choice',
+        props: {
+            txtMessage: String,
+            index: Number,
+        },
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .choice-div {
+        font-size: 16px;
+        color: #004444;
+    }
+
+    .selected {
+        font-weight: 800;
+    }
+</style>
---
+++ alphaM_with_vue_CLI/alpha-m-cli-app/src/components/QCM.vue
@@ -0,0 +1,37 @@
+<template>
+    <div v-on:click="$emit('choice-click',index)" class="hello">
+        <h1>{{ txtMessage }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        props: {
+            txtMessage: String,
+            index: String,
+            clickOnChoice: Function
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
